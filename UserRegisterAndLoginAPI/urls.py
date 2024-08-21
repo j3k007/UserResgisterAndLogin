f18888abd00a008cbba167b59b2e5667f93aabc3
Jatin Koli <70892665+j3k007@users.noreply.github.com>
2024-08-21 13:00:49 +0530

    User Login completed.

--- UserRegisterAndLoginAPI/urls.py
+++ UserRegisterAndLoginAPI/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path, include
+from .views import UserRegistration, UserLogin
 
-urlpatterns = []
+urlpatterns = [
+    path('register/', UserRegistration.as_view(), name='Register'),
+    path('login/', UserLogin.as_view(), name='Login')
+]
